24901dbf36bb2f71bcda6ff031402bf832f769e6
Susanna Sjöberg <susannasj@MacBook-Pro-som-tillhor-Administrator.local>
2021-10-22 10:54:21 +0200

    changed button background

--- src/components/ExpensesForm.js
+++ src/components/ExpensesForm.js
@@ -28,11 +28,13 @@ const expensesType = [
 
 const useStyles = makeStyles({
     button: {
-        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+        //background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+        backgroundColor: 'black !important',
         border: 0,
         borderRadius: 3,
         boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
         color: 'white',
+        fontWeight: 'bold',
         height: 48,
         marginTop: '5% !important',
         padding: '0 30px',
